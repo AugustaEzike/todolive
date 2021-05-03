d86a5da1e0859c57ea5024c62714fc5700168fd0
Augusta Ezike <augusta.ezike@yahoo.com>
2021-05-03 07:48:53 -0600

    pushing to github

--- public/main.js
+++ public/main.js
@@ -1,26 +1,87 @@
+//PUT request is sent when the edit-btn is clicked. enter event listeners
 
+// const editBtn = document.querySelectorAll(".fa-pen")
 
-// PUT
-// const update = document.querySelector('.completed')
+const deleteBtn = document.querySelectorAll(".fa-trash")
 
-// update.addEventListener('click', updateOne)
+const completedMission = document.querySelectorAll(".mission span")
+const uncompletedMission = document.querySelectorAll(".mission span.completed")
 
-// function updateOne() => {
-//     //send PUTrequest here
-//     fetch('/todo' {
-//         method: 'put'
-//     })
-// }
+//trigger PUT request with fetch API. fetch(endpoint, options)
+// edit.addEventListener('click', update => {{
+//     //everytime i click the edit-btn, it creates a new form and updates previous item
+    
+// })
 
+//DELETE added event listener to the delete elements
 
-//DELETE
-
-const deleteBtn = doocument.querySelectorAll("delete-btn")
 Array.from(deleteBtn).forEach((el) => {
-    el.addEventListener('click', deleteToDo)
+    el.addEventListener('click', deleteMission)
 })
 
-function deleteTodo() {
-    alert("you clicked delete");
+async function deleteMission(){
+    const missionText = this.parentNode.childNodes[0].mission
+    try {
+        const response = await fetch('deleteMission', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'allMissions': missionText
+            })
+        })
+        const data = await response.json()
+        console.log(missionText)
+        location.reload()
+    } catch (error) {
+        console.log(error)
+    }
 }
-    
+
+
+//to mark mission completed 
+
+Array.from(completedMission).forEach((el) => {
+    el.addEventListener('click', markComplete)
+})
+
+async function markComplete(){
+    const missionText = this.parentNode.childNodes[0].mission
+    try {
+        const response = await fetch('completeMission', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'allMissions': missionText
+            })
+        })
+        const data = await response.json()
+        console.log(missionText)
+        location.reload()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+//to mark uncompleted
+Array.from(uncompletedMission).forEach((el) => {
+    el.addEventListener('click', undo)
+})
+
+async function undo(){
+    const missionText = this.parentNode.childNodes[0].mission
+    try {
+        const response = await fetch('undo', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'allMissions': missionText
+            })
+        })
+        const data = await response.json()
+        console.log(missionText)
+        location.reload()
+    } catch (error) {
+        console.log(error)
+    }
+}
--- server.js
+++ server.js
@@ -1,64 +1,90 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
-const mongoClient = require('mongodb').MongoClient;
-const { MongoClient } = require('mongodb');
-
-const connectionString = 'mongodb+srv://Austa:Austa01@cluster0.l1oqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const MongoClient = require('mongodb').MongoClient
+const connectionString = 'mongodb+srv://reqe:thisisapassword@cluster0.l1oqp.mongodb.net/mytodoDB?retryWrites=true&w=majority'
 
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        //MAKE SURE BODY PARSER IS PUT BEFORE CRUD HANDLERS!!!
+        // urlencoded method within the body-parser tells body-parser to extract data from the <form> element and add them to the <body> property in the request object
+        app.use(bodyParser.urlencoded({ extended: true })) //urlencoded(options?: bodyParser.OptionsUrlencoded): createServer.NextHandleFunction. Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
+        app.use(express.static('public'))
+        app.use(bodyParser.json())
 
-// always use body-parser before your CRUD handlers
-//The urlencoded method within body-parser tells body-parser to extract data from the <form> element and
-app.use(bodyParser.urlencoded({ extended: true }))
+        
+        const db = client.db('to-do-missions')
+        const missionBoxes = db.collection('missions')
+        app.set('view engine', 'ejs')
 
-
-// CRUD HANDLERS
-app.listen(5000, () => console.log('listening on this port'))
-  
-
-//MONGODOB + CRUD
-
-MongoClient.connect(connectionString, {  useUnifiedTopology: true})
-  .then(client => {
-    // console.log('Connectd to the Database')
-    const db = client.db("to-do-items") // to-do-items is the name of the database
-    const todolist = db.collection('todos')
-
-    
-  
-    app.get('/', (req, res) => {
-      todolist.find().toArray()
-        .then(results => {
-          res.render('index.ejs', { todos: results }) //respond with a rendered ejs file
+        app.listen(3000, function() {
+            console.log("Listening on 3000")
         })
-        .catch(error => console.error(error))
-     
+        
+        //READ REQUEST (get a response from sever and read it back to me)
+        //app.get(endpont(where are we listening on, its the value that comes after the domain name), callback(tells the broswer what to do when the requested endpoint matches the value requested, it takes the request and response objects))
+        app.get('/', async (req, res) => {
+            const missionsList = await missionBoxes.find().toArray()
+            const missionsLeft = await missionBoxes.countDocuments({completed: false})
+            const missionsCount = await missionBoxes.countDocuments()
+            const missionsDone = await missionBoxes.countDocuments({completed: true})
+            res.render('index.ejs', {missions: missionsList, 
+                                        left: missionsLeft, 
+                                        all: missionsCount,
+                                        done: missionsDone})
+            // missionBoxes.find().toArray()
+            //     .then(results => {
+            //         res.render('index.ejs', { missions: results })   
+            //     })
+            //     .catch(error => console.error(error))
+        })
+        //__dirname is the name of current directory
+        //CREATE REQUEST (send something to the server)
+        //a CREATE operation is performed if browser sends a POST request to the server. either through js or through <form> in html. now i am posting to the endpoint /todos which corresponds to the /todos (and POST method) in html
+        app.post('/mission', (req, res) => {
+            missionBoxes.insertOne({mission: req.body.mission, completed: false})
+                .then(results => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+
+        //UPDATE (when you whant to PUT(change) something. Also triggered through js or <form> element)
+
+        //by convention UPDATE or PUT requests exist in a seperate folder called public
+        //create a server that browsers can listen to
+        app.put("/completeMission", (req, res) => {
+            missionBoxes.updateOne({missionText: req.body.allMissions}, {
+                $set: {
+                    completed: true
+                }
+            })
+            .then(result => {
+                console.log(this.parentNode.childNodes[0].innerText)
+                console.log('Marked Complete')
+                res.json('marked complete')
+            })
+        })
+
+        app.put("/undo", (req, res) => {
+            missionBoxes.updateOne({missionText: req.body.allMissions}, {
+                $set: {
+                    completed: false
+                }
+            })
+            .then(result => {
+                console.log('Marked uncomplete')
+                res.json('marked uncomplete')
+            })
+        })
+
+        app.delete('/deleteMission', (req, res) => {
+            
+            missionBoxes.deleteOne({missionText: req.body.allMissions})
+            .then(results => {
+                console.log('this is deleted')
+                res.json('Deleted it')
+            })
+        })
     })
-  
-  
-    app.post('/todo', (req, res) => {
-      todolist.insertOne(req.body)
-      .then(result => {
-          res.redirect('/')
-      })
-      .catch(error => console.error(error))
-    })
-
-})
-.catch(console.error)
-
-  
-
-    
-
-
-    
-
-
-
-
-
-  
